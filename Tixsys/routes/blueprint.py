bd56fb9e086d2fed39a784729748fe89b877eeb2
BS4yson <124108572+BS4yson@users.noreply.github.com>
2023-03-20 15:21:53 +0800

    Update blueprint.py

--- Tixsys/routes/blueprint.py
+++ Tixsys/routes/blueprint.py
@@ -6,17 +6,21 @@ from controller.dashboard import dashboard_system
 from controller.settings import settings_system
 
 bp = Blueprint('bp', __name__)
-#bp.route('/dashboard/settings', methods=['GET', 'PATCH'])(settings_system)
-bp.route('dashboard/settings/archived_user', methods=['PATCH'])(archived_user)
+
+#settings
+bp.route('dashboard/settings/archive_user', methods=['PATCH'])(archive_user)
 bp.route('dashboard/settings/logout', methods=['PATCH'])(logout_user)
-#bp.route('/login', methods=['POST'])(login_system)
+
+#login system
 bp.route('/login/login_user', methods=['POST'])(verify_user)
 bp.route('/login/register_user', methods=['POST'])(register_user)
-#bp.route('/dashboard', methods=['GET', 'POST', 'PATCH'])(dashboard_system)
+
+#dashboard
 bp.route('/dashboard/open_dashboard', methods=['GET'])(open_dashboard)
 bp.route('/dashboard/create_project', methods=['POST'])(create_project)
 bp.route('/dashboard/archive_project', methods=['PATCH'])(archive_project)
-#bp.route('/dashboard/<string:project_name>', methods=['GET', 'POST', 'PATCH'])(project_system)
+
+#project
 bp.route('/dashboard/<string:project_name>/create_project', methods=['POST'])(create_project)
 bp.route('/dashboard/<string:project_name>/open_project', methods=['GET'])(open_project)
 bp.route('/dashboard/<string:project_name>/archive_project', methods=['PATCH'])(archive_project)
@@ -24,7 +28,8 @@ bp.route('/dashboard/<string:project_name>/modify_project', methods=['PATCH'])(m
 bp.route('/dashboard/<string:project_name>/create_task', methods=['POST'])(create_task)
 bp.route('/dashboard/<string:project_name>/archive_task', methods=['PATCH'])(archive_task)
 bp.route('/dashboard/<string:project_name>/move_task', methods=['PATCH'])(move_task)
-#bp.route('/dashboard/<string:project_name>/<string:task_name>', methods=['GET', 'POST', 'PATCH'])(task_system)
+
+#task
 bp.route('/dashboard/<string:project_name>/<string:task_name>/create_task', methods=['POST'])(create_task)
 bp.route('/dashboard/<string:project_name>/<string:task_name>/open_task', methods=['GET'])(open_task)
 bp.route('/dashboard/<string:project_name>/<string:task_name>/archive_task', methods=['PATCH'])(archive_task)
